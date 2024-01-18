caee43f9d06363a9c9f012c326d7c0e294536d83
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-01-17 21:33:28 -0300

    [add]: connect sql server db

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -3,7 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvc")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
